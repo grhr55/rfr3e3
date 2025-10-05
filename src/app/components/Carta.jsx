1ca7751bbdc367c86bb7576780e5e2e9a8c26aff
kali <kali@kali>
2025-10-05 03:13:12 +0300

    Обновление проекта: все изменения

--- src/app/components/Carta.jsx
+++ src/app/components/Carta.jsx
@@ -1,27 +1,32 @@
+import { YMaps, Map ,Placemark } from "@pbe/react-yandex-maps";
 
-
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-
-export default function App() {
+export default function MyMap() {
   return (
-    <YMaps query={{ apikey: "0d853250-dd60-4ee7-a243-538bb1ae69d6", lang: "ru_RU" }}>
-      <Map 
-        defaultState={{ center: [50.4501, 30.5234], zoom: 10 }} 
-        width="100%" 
-        height="400px"
-      >
-        <Placemark
-          geometry={[50.4501, 30.5234]}
-          properties={{
-            balloonContent: "Привет из Киева",
-            hintContent: "Я метка"
-          }}
-          options={{
-            preset: "islands#redIcon",
-            draggable: true
-          }}
-        />
-      </Map>
-    </YMaps>
+<YMaps query={{ apikey: "0d853250-dd60-4ee7-a243-538bb1ae69d6" }}>
+  <Map
+    defaultState={{
+      center: [56.143151, 47.251102],
+      zoom: 17,
+      controls: ["zoomControl"],
+    }}
+    modules={["control.ZoomControl"]}
+    width="100%"
+    height="400px"
+    options={{
+      zoomControlSize: "small",
+      zoomControlFloat: "left",
+      zoomControlPosition: { top: 150, left: 10 },
+    }}
+  >
+    <Placemark
+      geometry={[56.143151, 47.251102]}
+      properties={{
+        balloonContent: "428000, г. Чебоксары, Марпосадское шоссе, 14",
+        hintContent: "Марпосадское шоссе, 14",
+      }}
+    />
+  </Map>
+</YMaps>
+
   );
 }
