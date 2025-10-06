e8b01d4336f474f46216021a846312568c70bc7b
kali <kali@kali>
2025-10-06 17:18:44 +0300

    Обновление проекта: все изменения

--- src/app/components/Carta.jsx
+++ src/app/components/Carta.jsx
@@ -1,32 +1,33 @@
-import { YMaps, Map ,Placemark } from "@pbe/react-yandex-maps";
+
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Создаём кастомную иконку
+const customIcon = L.icon({
+  iconUrl: '/img/placeholder 1.png', // URL на изображение
+  iconSize: [40, 40], // размер изображения (ширина, высота)
+  iconAnchor: [20, 40], // точка "якоря" (относительно левого верхнего угла)
+  popupAnchor: [0, -40], // смещение для popup (если нужно)
+});
 
 export default function MyMap() {
-  return (
-<YMaps query={{ apikey: "0d853250-dd60-4ee7-a243-538bb1ae69d6" }}>
-  <Map
-    defaultState={{
-      center: [56.143151, 47.251102],
-      zoom: 17,
-      controls: ["zoomControl"],
-    }}
-    modules={["control.ZoomControl"]}
-    width="100%"
-    height="400px"
-    options={{
-      zoomControlSize: "small",
-      zoomControlFloat: "left",
-      zoomControlPosition: { top: 150, left: 10 },
-    }}
-  >
-    <Placemark
-      geometry={[56.143151, 47.251102]}
-      properties={{
-        balloonContent: "428000, г. Чебоксары, Марпосадское шоссе, 14",
-        hintContent: "Марпосадское шоссе, 14",
-      }}
-    />
-  </Map>
-</YMaps>
+  const position = [55.547458, 37.772559]; // Киев, например
 
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; OpenStreetMap'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      
+      <Marker position={position} icon={customIcon}>
+        <Popup>
+          <b>Пример фото-метки</b><br />
+          Здесь можно описать объект.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
