19dca2c9bc0d8c9e27cf95289825fd6e4a443854
kali <kali@kali>
2025-10-07 22:19:12 +0300

    Обновление проекта: все изменения

--- src/app/Error/page.js
+++ src/app/Error/page.js
@@ -1,6 +1,8 @@
 
 'use client'
 
+import Link from 'next/link';
+
 
 export default function Erro() {
     return (
@@ -14,15 +16,16 @@ export default function Erro() {
   <p className="text-[22px] text-amber-50 mb-8">
     Страница не найдена
   </p>
-
-  <a
-    href="/pages"
+ <Link
+    href="/"
     className="w-[295px] h-[87px] py-[20px] px-[30px] mt-[30px] cursor-pointer font-black rounded-[11px]
                bg-[url('/img/fe.png')] bg-no-repeat bg-cover bg-center
                text-white text-[22px] shadow-[inset_0_4px_12px_0_rgba(0,0,0,0.45)] transition duration-300 hover:opacity-90"
   >
     Вернуться на сайт
-  </a>
+  </Link>
+
+ 
 </div>
 
  </div>
